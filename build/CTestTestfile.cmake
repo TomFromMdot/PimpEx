84b8e778a9d200b417daffa2397a450d4df8d929
Tommy <69269534+TomFromMdot@users.noreply.github.com>
2026-04-08 15:44:45 +0200

    Delete build directory
    Clearing

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/Tomasz/live_content/git_workspace/PimpEx
-# Build directory: /home/Tomasz/live_content/git_workspace/PimpEx/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
